60339db6d7ab72d5c85ec6a9ff4e8cbc10b0df3b
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-10 12:57:01 -0400

    added connections

--- config/connection.js
+++ config/connection.js
@@ -13,3 +13,21 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+// const Sequelize = require('sequelize');
+ // require('dotenv').config();
